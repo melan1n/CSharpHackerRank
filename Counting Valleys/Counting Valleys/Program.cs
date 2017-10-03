8b2383dbaee3b53d9239002f533c1d79b470551a
MilenN <milennikolov_bg@yahoo.com>
2017-10-03 22:22:04 +0300

    Add Drawing Book and Counting Valleys

---
+++ Counting Valleys/Counting Valleys/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counting_Valleys
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine());
+			string steps = Console.ReadLine();
+			int[] stepsDirections = new int[n];   //create an int array for steps (+/-1)
+
+			for (int i = 0; i < n; i++)
+			{
+				if (steps[i] == 'U')
+				{
+					stepsDirections[i] = 1;
+				}
+				else
+				{
+					stepsDirections[i] = -1;
+				}
+			}
+			int valleyCount = 0;				//have a counter of valleys
+			int levelAtPreviousStep = 0;        //have a previous-step level meter
+			int levelAtCurrentStep = 0;         //have a current-step level meter
+			for (int i = 0; i < n; i++)         //track all occurrences of negative to 0 level and increment valley counter 
+			{
+				levelAtCurrentStep = levelAtPreviousStep + stepsDirections[i];
+				if (levelAtPreviousStep < 0 && levelAtCurrentStep == 0)
+				{
+					valleyCount++;
+				}
+				levelAtPreviousStep = levelAtCurrentStep;
+			}
+			Console.WriteLine(valleyCount);
+		}
+	}
+}
---
+++ Drawing Book/Drawing Book/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drawing_Book
+{
+	class Program
+	{
+		static int solve(int n, int p)
+		{
+			// Complete this function
+			int pagesTurnsFromStart = 0 ;
+			int pagesTurnsFromEnd = 0;
+			if (p % 2 == 1)
+			{
+				pagesTurnsFromStart = (p - 1)/2;
+				if (n % 2 == 0)
+				{
+					pagesTurnsFromEnd = (int)Math.Ceiling((double)((double)n - p) / 2);
+				}
+				else
+				{
+					pagesTurnsFromEnd = (n - p) / 2;
+				}
+			}
+			else if (p % 2 == 0)
+			{
+				pagesTurnsFromStart = p / 2;
+				if (n % 2 == 0)
+				{
+					pagesTurnsFromEnd = (n - p) / 2;
+				}
+				else
+				{
+					pagesTurnsFromEnd = (n - 1 - p) / 2;
+				}
+			}
+			return (int)Math.Min(pagesTurnsFromStart, pagesTurnsFromEnd);
+		}
+
+		static void Main(String[] args)
+		{
+			int n = Convert.ToInt32(Console.ReadLine());
+			int p = Convert.ToInt32(Console.ReadLine());
+			int result = solve(n, p);
+			Console.WriteLine(result);
+		}
+
+	}
+}
